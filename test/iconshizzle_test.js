f1466832c6851e761a49f4cc5d053a6e0b7ef4f7
dapenguin <anthony.jeffery@littleblueweb.com>
2015-07-22 08:14:10 +0100

    Now creates PNGs and works with Promises to work out when they have been created. Added a unit test to check whether PNGs were created.

--- test/iconshizzle_test.js
+++ test/iconshizzle_test.js
@@ -7,17 +7,17 @@ var IconShizzle = require('../lib/iconshizzle.js');
 
 // Set up our parameters for IconShizzle
 var svgLocation = path.join(__dirname,'svg');
-var actualSassFile = path.join(__dirname,'../tmp/testSassSvg.scss');
+// var actualSassFile = path.join(__dirname,'../tmp/testSassSvg.scss');
 var options = {
-    actualSassFile: 'example/output/result.css',
-    svgCssOptions: {
+	actualSassFile: 'tmp/output/result.css',
+	svgCssOptions: {
 		prefix: ".icon-", // used to prefix the name of the file for the CSS classname, .icon- is the default
 		templatePrepend: "/* Start of SVG icons */\n", // this string is prepended to the destinationCSSFile, defaults to ""
 		template: path.join(__dirname,'templates/default-css.hbs'), //template in handlebars, FANCY!
 		templateAppend: "/* End of SVG icons */" // this string is appended to the destinationCSSFile, defaults to ""
-    },
-    pngFileOptions: {
-    	outputFolder: 'example/output/',
+	},
+	pngFileOptions: {
+		outputFolder: 'tmp/output/',
 		compress: false,
 		optimizationLevel: 3,
 		dimensions: [
@@ -30,8 +30,10 @@ var options = {
 				height: "40px"
 			}
 		]
-    },
-    pngSpriteOptions: {}
+	},
+	pngSpriteOptions: {
+
+	}
 };
 
 // Variables for storing the actual and expected output of IconShizzle
@@ -40,17 +42,23 @@ var expectedOutput;
 var expectedSassFile = path.join(__dirname,'expected/testSassSvg.scss');
 
 // Create an instance of IconShizzle for our tests
-var iShizz = new IconShizzle(svgLocation,actualSassFile,options);
-
-iShizz.process();
-
-actualOutput = fs.readFileSync(actualSassFile,{encoding: 'utf-8'});
-expectedOutput = fs.readFileSync(expectedSassFile,{encoding: 'utf-8'});
+var iShizz = new IconShizzle(svgLocation, options);
 
 exports.iconshizzle = {
 	'Correct SASS output' : function(test){
 		test.expect(1);
-		test.equal(actualOutput,expectedOutput,'Should give us the expected output in the generated SASS file');
-		test.done();
+		iShizz.process(function(){
+			actualOutput = fs.readFileSync(options.actualSassFile,{encoding: 'utf-8'});
+			expectedOutput = fs.readFileSync(expectedSassFile,{encoding: 'utf-8'});
+			test.equal(actualOutput,expectedOutput,'Should give us the expected output in the generated SASS file');
+			test.done();
+		});
+	},
+	'PNG created' : function(test){
+		test.expect(1);
+		iShizz.process(function(){
+			test.ok(fs.existsSync(path.join(options.pngFileOptions.outputFolder,'w400px_h300px','MapPin.png')),'Should give us one of the PNG files created');
+			test.done();
+		});
 	},
 };
